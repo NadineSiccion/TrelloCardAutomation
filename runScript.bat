89aa2e704d0c89ceb3d4d7d43bad44e3d34bf9f8
Nadine Siccion <nv.siccion@gmail.com>
2025-01-19 02:16:59 +0800

    Automate venv creation and pip install in script

--- runScript.bat
+++ runScript.bat
@@ -1,2 +1,31 @@
 @echo off
-call start notepad "input_data.txt" && call venv\Scripts\activate.bat && python main.py
+
+echo setting up virtual environment, this may take a upto 3 minutes...
+
+:: Step 1: Run 'venv' to create a virtual environment in the same folder
+python -m venv ve
+if %errorlevel% neq 0 (
+    echo Failed to create virtual environment.
+    pause
+    exit /b
+)
+
+:: Step 3: Activate the virtual environment
+call ve\Scripts\activate.bat
+if %errorlevel% neq 0 (
+    echo Failed to activate virtual environment.
+    pause
+    exit /b
+)
+
+:: Step 4: Install packages from requirements.txt
+pip install -r requirements.txt
+if %errorlevel% neq 0 (
+    echo Failed to install packages from requirements.txt.
+    pause
+    exit /b
+)
+
+:: Step 5: Run main.py and input_data.txt
+cls
+call start notepad "input_data.txt" && python main.py
